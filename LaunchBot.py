2b2c5c171bf57c258b377a51959d6902eb7d70e3
Hugo Blair <blairhugo@ecs.vuw.ac.nz>
2024-06-02 01:40:37 +1200

    Changes to regex and comment search

--- LaunchBot.py
+++ LaunchBot.py
@@ -6,7 +6,7 @@ reddit = praw.Reddit('launchBot')
 
 subreddit = reddit.subreddit("pythonforengineers")
 
-commentPattern = re.compile("When is rocketlab's next launch?", re.IGNORECASE)
+commentPattern = re.compile("(when is|when's|when does)\s+(rocketlab|rocketlab's)\s+launch", re.IGNORECASE)
 
 if not os.path.isfile("comments_replied_to.txt"):
     comments_replied_to = []
@@ -17,16 +17,16 @@ else:
        comments_replied_to = list(filter(None, comments_replied_to))
 
 
-for submission in subreddit.new(limit=10):
-    for comment in submission.comments:
+    for comment in subreddit.stream.comments():
         
         print("Found new comment")
         if commentPattern.search(comment.body):
             print("Found a comment asking about Rocket Lab's next launch")
-            if(comment.id not in comments_replied_to):
-                comments_replied_to.append(comment.id)
-                print("Replied to:"+comment.id)
-                comment.reply("Rocket Lab's next launch is scheduled for August 19th, 2019. You can find more information on the Rocket Lab website: https://www.rocketlabusa.com/launch/")
+            if comment.author.name != reddit.user.me().name:
+                if(comment.id not in comments_replied_to):
+                    comments_replied_to.append(comment.id)
+                    print("Replied to:"+comment.id)
+                    comment.reply("Rocket Lab's next launch is scheduled for August 19th, 2019. You can find more information on the Rocket Lab website: https://www.rocketlabusa.com/launch/")
 
 
 with open("comments_replied_to.txt", "w") as f:
